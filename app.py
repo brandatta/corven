6b5ddd3197a071a821df4d7774fcb0deb7dd8e1a
brandatta <87575253+brandatta@users.noreply.github.com>
2025-11-14 12:57:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ else:
     total_row = pd.DataFrame({
         "Sociedad": ["TOTAL"],
         **{b: [tabla_soc[b].sum()] for b in buckets},
-        "Subotal": [tabla_soc["Total"].sum()]
+        "TOTAL": [tabla_soc["Total"].sum()]
     })
 
     tabla_soc_total = pd.concat([tabla_soc, total_row], ignore_index=True)
